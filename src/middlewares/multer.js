f612e641cf16e3aa1b5345c2b1b56ed688f450d3
= <nodejsexperiments@gmail.com>
2024-09-03 22:56:50 +0530

    multer setup and cloudinary setup

---
+++ src/middlewares/multer.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+// Set up storage with Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../../public/temp/'); // Directory where files will be stored
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original file name
+    }
+  });
+  
+  // Initialize Multer with the storage settings
+  const upload = multer({ storage: storage });
+  
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,64 +1,90 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const userSchema = Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true, 
-            index: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowecase: true,
-            trim: true, 
-        },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true, 
-            index: true
-        },
-        avatar: {
-            type: String, // cloudinary url
-            required: true,
-        },
-        coverImage: {
-            type: String, // cloudinary url
-        },
-        watchHistory: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        ],
-        password: {
-            type: String,
-            required: [true, 'Password is required']
-        },
-        refreshToken: {
-            type: String
-        }
-
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 userSchema.pre("Save", async function (next) {
-    if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 12)
-    next()
-})
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 12);
+  next();
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
-export const User = mongoose.model('User', userSchema);
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+export const User = mongoose.model("User", userSchema);
