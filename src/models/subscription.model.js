3856a40cf1030345a5cc9057c6e5e40de5a89e35
= <nodejsexperiments@gmail.com>
2024-09-17 00:48:18 +0530

    subsciption model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,17 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({}, {})
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    
+}, {
+    timestamps: true
+});
 
 export const subscriptionModel = mongoose.model("Subscription", subscriptionSchema)
