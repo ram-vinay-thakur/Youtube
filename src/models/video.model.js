0fe9d2c31ee7bccbf295fe8ace3e01f21fccb701
= <nodejsexperiments@gmail.com>
2024-09-16 23:28:10 +0530

    k

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -41,5 +41,5 @@ const videoSchema = new Schema(
     }
 )
 
-videoSchema.plugin(mongooseAggregatePaginate)
-export const Video = mongoose.model("Video", videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
